f02b0c1ceec990fd5dc5b4e185cd600edc9813a3
Philip N. Brown <philip.brown@gatech.edu>
2016-07-05 12:17:43 -0700

    added loader method to montecarlo.py as well as some tidbits

--- montecarlo.py
+++ montecarlo.py
@@ -9,6 +9,27 @@ from linear import fourPathGeneric1
 import numpy.random
 from numpy import array, reshape
 
+def loadNet(net) :
+    global M
+    global R
+    global XAXT
+    global XAXTi
+    global n
+    global X
+    global Y
+    global A
+    global b
+    M = net.M
+    R = net.R
+    XAXT = net.Y@net.A@net.Y.T 
+    XAXTi = numpy.linalg.inv(XAXT)
+    n = net.n
+    X = net.X
+    Y = net.Y
+    A = net.A
+    b = net.b
+
+
 numIter = 1
 badNets = []
 z23 = reshape(array([0,1,1]),[1,3])
@@ -29,4 +50,19 @@ for i in range(numIter):
     except Exception as err:
         badNets.append({'a': a, 'net':None, 'err':err, 'permute':permute})
     
-XAXT = testNet.Y@testNet.A@testNet.Y.T 
+#XAXT = testNet.Y@testNet.A@testNet.Y.T 
+#XAXTinv = numpy.linalg.inv(XAXT)
+
+# these zt's correspond to CMC tolls:
+zt1 = reshape(array([.9,.8,.7]),[3,1])
+zt2 = reshape(array([.8,.7,.6]),[3,1])
+zt3 = reshape(array([.99,.75,.51]),[3,1])
+zt4 = reshape(array([.95,.75,.55]),[3,1])
+
+zMC = reshape(array([.5,.5,.5]),[3,1])
+
+# these zt's correspond to non-CMC tolls:
+zt5 = reshape(array([.9,.8,.4]),[3,1])
+zt6 = reshape(array([.99,.5,.01]),[3,1])
+zt7 = reshape(array([.7,.6,.3]),[3,1])
+zt8 = reshape(array([.6,.3,.2]),[3,1])
