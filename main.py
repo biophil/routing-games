5081420cf5b07e9a9bdebc8574dae760f61ee72b
Philip N. Brown <philip.brown@gatech.edu>
2016-09-02 17:05:17 -0700

    edited main.py to include a hypothetical bad example for weak robustness

--- main.py
+++ main.py
@@ -9,8 +9,20 @@ import numpy as np
 from linear import Network
 import numpy.linalg as la
 
-t = Network(np.diag([1,2,3,4]),[1,1.5,3,4.5])
-print(t.X)
+#t = Network(np.diag([1,2,3,4]),[1,1.5,3,4.5])
+#print(t.X)
+
+base = np.zeros([6,6])
+base[0:3,0:3] = np.diag(np.ones(3))
+base[0,0:3] = base[0,0:3] + np.ones(3)
+base[0:3,0] = base[0:3,0] + np.ones(3)
+#base[1,0:2] = 2*np.ones(2)
+#base[0,1] = 2
+base[3:6,3:6] = np.flipud(np.fliplr(base[0:3,0:3]))
+
+b = range(0,6)
+
+t = Network(base,b)
 
 XAXT = t.Y@t.A@t.Y.T # this is the matrix that tells us which direction z moves
 # (if all b(i+1)-b(i)=1)
