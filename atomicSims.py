b10380162fee0e908908d450b36bd136f0f83091
Philip N. Brown <philip.brown@gatech.edu>
2016-09-27 07:16:30 -0700

    modified the simulation script

--- atomicSims.py
+++ atomicSims.py
@@ -10,7 +10,7 @@ import numpy.random as rnd
 from atomic.classes import *
 
 population = {}
-population[0] = 333
+population[0.1] = 333
 population[0.5] = 333
 population[1] = 334
 #numPlayers = 1000
@@ -26,15 +26,18 @@ for sens in population :
 #    players.append(Player())
 
 costs = []
-costs.append(lambda x,s : (1+s)/200*x)
-costs.append(lambda x,s : (1+s)/200*x + 1)
+#costs.append(lambda x,s : (1+s)/200*x)     # mc toll
+#costs.append(lambda x,s : (1+s)/200*x + 1) # mc toll
+#costs.append(lambda x,s : 2)
+costs.append(lambda x,s : x/200 + s*10)        # fixed toll of tau_1 = 10
+costs.append(lambda x,s : x/200 + 1 + s*5)# fixed toll of tau_2 = 5
 costs.append(lambda x,s : 2)
 
 testG = Game(players,costs)
 
 
 numIter = 10000
-noChangeThresh = 100
+noChangeThresh = 10000
 noChange = 0
 
 for i in range(0,numIter) :
@@ -53,3 +56,10 @@ for i in range(0,numIter) :
         noChange = 0
     if noChange > noChangeThresh :
         break
+    
+players_actions = {0:[], 1:[], 2:[]}
+for player in testG.players :
+    players_actions[player.currentAction].append(player.sensitivity)
+print('types on path 0 :' + str(set(players_actions[0])))
+print('types on path 1 :' + str(set(players_actions[1])))
+print('types on path 2 :' + str(set(players_actions[2])))
